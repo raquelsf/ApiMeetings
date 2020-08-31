beb0f4dfd8c050726d0dbbbfa3c26e399b4a96c4
Raquel <raquelsfreitasmaia@gmail.com>
2020-08-31 10:20:18 -0300

    adicionado outras models

--- Migrations/20200606173339_Initial.cs
+++ Migrations/20200606173339_Initial.cs
@@ -3,32 +3,65 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ApiMeetings.Migrations
 {
-    public partial class Initial : Migration
+  public partial class Initial : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Rooms",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(nullable: false),
-                    Name = table.Column<string>(maxLength: 30, nullable: false),
-                    Description = table.Column<string>(maxLength: 100, nullable: false),
-                    Floor = table.Column<int>(nullable: false),
-                    MaxPeople = table.Column<int>(nullable: false),
-                    HasTV = table.Column<bool>(nullable: false),
-                    HasPainting = table.Column<bool>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Rooms", x => x.Id);
-                });
-        }
+      migrationBuilder.CreateTable(
+          name: "Rooms",
+          columns: table => new
+          {
+            Id = table.Column<Guid>(nullable: false),
+            Name = table.Column<string>(maxLength: 30, nullable: false),
+            Description = table.Column<string>(maxLength: 100, nullable: false),
+            Floor = table.Column<int>(nullable: false),
+            MaxPeople = table.Column<int>(nullable: false),
+            HasTV = table.Column<bool>(nullable: false),
+            HasPainting = table.Column<bool>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Room", x => x.Id);
+          });
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Rooms");
-        }
+      migrationBuilder.CreateTable(
+         name: "Reservations",
+         columns: table => new
+         {
+           Id = table.Column<Guid>(nullable: false),
+           UserId = table.Column<Guid>(nullable: false),
+           RoomId = table.Column<Guid>(nullable: false),
+           DateInitial = table.Column<DateTime>(nullable: false),
+           DateEnd = table.Column<DateTime>(nullable: false)
+         },
+         constraints: table =>
+         {
+           table.PrimaryKey("PK_Reservation", x => x.Id);
+           table.PrimaryKey("PK_Reservation_User", x => x.UserId);
+           table.PrimaryKey("PK_Reservation_Room", x => x.RoomId);
+         });
+
+      migrationBuilder.CreateTable(
+         name: "users",
+         columns: table => new
+         {
+           Id = table.Column<Guid>(nullable: false),
+           Name = table.Column<string>(maxLength: 30, nullable: false),
+         },
+         constraints: table =>
+         {
+           table.PrimaryKey("PK_User", x => x.Id);
+         });
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+          name: "Rooms");
+      migrationBuilder.DropTable(
+         name: "Reservations");
+      migrationBuilder.DropTable(
+         name: "Users");
+    }
+  }
 }
---
+++ Model/ReservationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiMeetings.Model
+{
+  public class ReservationModel
+  {
+    [Key]
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+    public Guid UserId { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+    public Guid RoomId { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+    public DateTime DateInitial { get; set; }
+
+
+    [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+    public DateTime DateEnd { get; set; }
+  }
+}
